80ab676d8184f496690f97c2cde985c1f0b1075d
love-ara <aramideotenaike@gmail.com>
2024-04-12 08:18:22 +0100

    commit

--- src/javaScript/reverseAnArray.js
+++ src/javaScript/reverseAnArray.js
@@ -1,5 +1,13 @@
 
 function reverseArray(numbers){
+
+    for(let number = 0, j = numbers.length-1; number < j;number++, j--){
+        [numbers[number], numbers[j]] = [numbers[j], numbers[number]];
+    }
+    return numbers;
+}
+
+function reverseArray1(numbers){
     const reversedList= [];
 
     for(let number = numbers.length-1; number >= 0; number--){
@@ -8,6 +16,7 @@ function reverseArray(numbers){
     return reversedList;
 }
 
+
 console.log(reverseArray([1,2,3,4,5,6,7]));
-console.log(reverseArray([3,2,3,4,5,6,10]));
-console.log(reverseArray([30,2,43,7,15,6,10]));
+console.log(reverseArray1([3,2,3,4,5,6,10]));
+// console.log(reverseArray([30,2,43,7,15,6,10]));
