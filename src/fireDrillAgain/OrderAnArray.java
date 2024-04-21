30cf0a01aae4771c3513c961b9254d4f58c70cc2
love-ara <aramideotenaike@gmail.com>
2024-04-21 16:08:48 +0100

    commit

--- src/fireDrillAgain/OrderAnArray.java
+++ src/fireDrillAgain/OrderAnArray.java
@@ -1,22 +1,42 @@
 package fireDrillAgain;
 
-import java.util.Arrays;
 
 public class OrderAnArray {
 
     public int[] moveZeros(int[] arr) {
 
 
-        for (int firstInex = 0; firstInex < arr.length; firstInex++) {
-            for (int secondIndex = firstInex + 1; secondIndex < arr.length; secondIndex++) {
-            if (arr[firstInex] == 0) {
-                arr[firstInex] = arr[secondIndex];
+        for (int firstIndex = 0; firstIndex < arr.length; firstIndex++) {
+            for (int secondIndex = firstIndex + 1; secondIndex < arr.length; secondIndex++) {
+            if (arr[firstIndex] == 0) {
+                arr[firstIndex] = arr[secondIndex];
                 arr[secondIndex] = 0;
             }
         }
 
         }
-        System.out.println(Arrays.toString(arr));
+
         return arr;
         }
-}
+
+
+        public int[] sort(int[] arr) {
+            int count = 0;
+
+            for (int index = 0; index < arr.length; index++) {
+                if (arr[index] != 0) {
+                    arr[count++] = arr[index];
+                }
+            }
+            while (count < arr.length) {
+                arr[count++] = 0;
+            }
+            return arr;
+          }
+
+
+    }
+
+
+
+
