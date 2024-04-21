4dbc6f64ffa8fab6c452dd40a00f5b60cda68e74
love-ara <aramideotenaike@gmail.com>
2024-04-21 16:09:12 +0100

    commit

--- test/fireDrillAgain/OrderAnArrayTest.java
+++ test/fireDrillAgain/OrderAnArrayTest.java
@@ -5,13 +5,12 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OrderAnArrayTest {
     @Test
     public void testOrderAnArray() {
         OrderAnArray orderAnArray = new OrderAnArray();
-        int[] arr = { 3, 2, 0, -1, 7, 0, 8 };
+        int[] arr = { 0, 0, 3, 2, -1, 7, 8 };
         int[] expected = { 3, 2, -1, 7, 8, 0, 0};
         assertArrayEquals(expected, orderAnArray.moveZeros(arr));
     }
@@ -21,5 +20,13 @@ public class OrderAnArrayTest {
         int[] arr = { 0, 2, 0, -1, 7, 0, 8 };
         int[] expected = {2, -1, 7, 8, 0, 0, 0};
         assertArrayEquals(expected, orderAnArray.moveZeros(arr));
+    }
+    @Test
+    public void testOrderAnArrayThree() {
+        OrderAnArray orderAnArray = new OrderAnArray();
+        int[] arr = { 0, 2, 0, -1, 7, 0, 8 };
+        int[] expected = {2, -1, 7, 8, 0, 0, 0};
+        System.out.println(Arrays.toString(orderAnArray.sort(arr)));
+        assertArrayEquals(expected, orderAnArray.sort(arr));
     }
 }
