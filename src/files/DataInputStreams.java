7e3b58f68e652d69a87c989d9b7e187864d9a103
love-ara <aramideotenaike@gmail.com>
2024-04-30 10:06:34 +0100

    commit

---
+++ src/files/DataInputStreams.java
@@ -0,0 +1,15 @@
+package files;
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class DataInputStreams {
+    public static void main(String[] args) {
+        String file = "C:\\Users\\Administrator\\IdeaProjects\\javaStuff2\\heyBestie";
+        //InputStream inputStream = new FileInputStream()
+        //DataInputStream dataInputStream = new DataInputStream();
+    }
+}
+
+
