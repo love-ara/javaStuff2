16bd915459888f5e503f5f2d1276be7a42df1c97
love-ara <aramideotenaike@gmail.com>
2024-05-18 15:42:11 +0100

    commit

--- src/functionalInterface/examples/DoubleConsumerSample.java
+++ src/functionalInterface/examples/DoubleConsumerSample.java
@@ -1,4 +1,11 @@
 package functionalInterface.examples;
 
+import java.util.function.DoubleConsumer;
+
 public class DoubleConsumerSample {
+    public static void main(String[] args) {
+        DoubleConsumer doubleConsumer = number -> System.out.println(number + 1.5);
+
+        doubleConsumer.accept(2.5);
+    }
 }
