449e2589fc91b4916c8414ed149f0b52388f9172
love-ara <aramideotenaike@gmail.com>
2024-05-18 15:02:34 +0100

    commit

---
+++ src/functionalInterface/IntUnaryOperation.java
@@ -0,0 +1,10 @@
+package functionalInterface;
+
+import java.util.function.IntUnaryOperator;
+
+public class IntUnaryOperation {
+    public static void main(String[] args) {
+        IntUnaryOperator func = number -> number * 2;
+        System.out.println(func.applyAsInt(5));
+    }
+}
