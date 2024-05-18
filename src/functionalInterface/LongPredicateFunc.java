78d2e966a1db615a0304d81288fec3d0800c42df
love-ara <aramideotenaike@gmail.com>
2024-05-18 14:59:43 +0100

    commit

---
+++ src/functionalInterface/LongPredicateFunc.java
@@ -0,0 +1,13 @@
+package functionalInterface;
+
+import java.util.function.LongPredicate;
+
+public class LongPredicateFunc {
+    public static void main(String[] args) {
+        LongPredicate isEven = number -> number % 2 == 0;
+        LongPredicate isOdd = number -> number % 2 != 0;
+
+        System.out.println(isEven.test(2));
+        System.out.println(isOdd.test(11));
+    }
+}
