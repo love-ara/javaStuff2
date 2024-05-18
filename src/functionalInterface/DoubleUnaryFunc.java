925bb050a4dc07c8b50e498df4760276bec4c7a7
love-ara <aramideotenaike@gmail.com>
2024-05-18 14:44:11 +0100

    commit

---
+++ src/functionalInterface/DoubleUnaryFunc.java
@@ -0,0 +1,12 @@
+package functionalInterface;
+
+import java.util.function.DoubleUnaryOperator;
+
+public class DoubleUnaryFunc {
+    public static void main(String[] args) {
+        DoubleUnaryOperator operator = number -> number *3;
+
+
+        System.out.println(operator.applyAsDouble(10));
+    }
+}
