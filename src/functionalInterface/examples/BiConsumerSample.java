41cc230fb30c70221b15d317daf449d48e65a49e
love-ara <aramideotenaike@gmail.com>
2024-05-18 15:30:24 +0100

    commit

--- src/functionalInterface/examples/BiConsumerSample.java
+++ src/functionalInterface/examples/BiConsumerSample.java
@@ -1,4 +1,12 @@
 package functionalInterface.examples;
 
+import java.util.function.BiConsumer;
+
 public class BiConsumerSample {
+    public static void main(String[] args) {
+        BiConsumer<Integer, Integer> biConsumer = (x, y) -> {
+            System.out.println(x+y);
+        };
+        biConsumer.accept(10, 20);
+    }
 }
--- src/functionalInterface/examples/BiPredicateSample.java
+++ src/functionalInterface/examples/BiPredicateSample.java
@@ -1,4 +1,10 @@
 package functionalInterface.examples;
 
+import java.util.function.BiPredicate;
+
 public class BiPredicateSample {
+    public static void main(String[] args) {
+        BiPredicate<Integer, Integer> biPredicate = (argOne, argTwo) -> argOne > argTwo;
+        System.out.println(biPredicate.test(1, 2));
+    }
 }
