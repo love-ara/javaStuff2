b33278045410cca108b9ca58f6e4384048279ef2
love-ara <aramideotenaike@gmail.com>
2024-05-18 14:40:37 +0100

    commit

---
+++ src/functionalInterface/IntBinaryOperatorFunc.java
@@ -0,0 +1,10 @@
+package functionalInterface;
+
+import java.util.function.IntBinaryOperator;
+
+public class IntBinaryOperatorFunc {
+    public static void main(String[] args) {
+        IntBinaryOperator operator = (a, b) -> a + b;
+        System.out.println(operator.applyAsInt(1, 2));
+    }
+}
