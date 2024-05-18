32f14647b0af3088bf6a265c2e020a26a70e60c7
love-ara <aramideotenaike@gmail.com>
2024-05-18 15:32:40 +0100

    commit

--- src/functionalInterface/examples/BiFunctionSample.java
+++ src/functionalInterface/examples/BiFunctionSample.java
@@ -1,4 +1,14 @@
 package functionalInterface.examples;
 
+import java.util.function.BiFunction;
+
 public class BiFunctionSample {
+    public static void main(String[] args) {
+        BiFunction<Integer, Integer, Integer> add = (x, y) -> x + y;
+        BiFunction<Integer, Integer, Integer> multiply = (x, y) -> x * y;
+        BiFunction<Integer, Integer, Integer> divide = (x, y) -> x / y;
+        System.out.println(add.apply(10, 20));
+        System.out.println(multiply.apply(10, 20));
+        System.out.println(divide.apply(20, 10));
+    }
 }
--- src/functionalInterface/examples/BinaryOperatorSample.java
+++ src/functionalInterface/examples/BinaryOperatorSample.java
@@ -1,4 +1,10 @@
 package functionalInterface.examples;
 
+import java.util.function.BinaryOperator;
+
 public class BinaryOperatorSample {
+    public static void main(String[] args) {
+        BinaryOperator<Integer> sum = (firstNumber, secondNumber) -> firstNumber + secondNumber;
+        System.out.println(sum.apply(10, 20));
+    }
 }
