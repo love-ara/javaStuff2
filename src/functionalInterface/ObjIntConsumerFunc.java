b183e213d1b12f0f094c647b4c0be8e9d8e8c323
love-ara <aramideotenaike@gmail.com>
2024-05-18 14:55:38 +0100

    commit

---
+++ src/functionalInterface/ObjIntConsumerFunc.java
@@ -0,0 +1,13 @@
+package functionalInterface;
+
+import java.util.function.ObjIntConsumer;
+
+public class ObjIntConsumerFunc {
+    public static void main(String[] args) {
+        ObjIntConsumer<Object> objIntConsumer = (object, number)-> System.out.printf("%s is %d years old", object, number);
+
+        objIntConsumer.accept("Aramide", 2);
+    }
+
+
+}
