f1dd2bc576599a7e9d82f4ccf36f1ff3eba8a86c
love-ara <aramideotenaike@gmail.com>
2024-05-18 10:38:15 +0100

    commit

---
+++ src/functionalInterface/LongToIntFunc.java
@@ -0,0 +1,10 @@
+package functionalInterface;
+
+import java.util.function.LongToIntFunction;
+
+public class LongToIntFunc{
+    public static void main(String[] args) {
+        LongToIntFunction longToIntFunction = x -> (int) x;
+        System.out.println(longToIntFunction.applyAsInt(10L));
+    }
+}
